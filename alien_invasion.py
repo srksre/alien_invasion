e53ceeccb901b31e2c498998c9715122d2b5115d
srksre <srkfru87@gmail.com>
2022-10-12 12:59:37 +0530

    Project Started

---
+++ alien_invasion.py
@@ -0,0 +1,60 @@
+import sys
+import pygame
+
+from setting import Setting
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+        self.setting = Setting()
+
+        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+        # Set the background colour.
+        # self.bg_colour = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        # Watch the keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new session."""
+        self.screen.fill(self.setting.bg_colour)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ setting.py
@@ -0,0 +1,12 @@
+class Setting:
+    """A class to store all setting for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's setting."""
+        # Screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
+
+        # Ship setting
+        self.ship_speed = 1.5
